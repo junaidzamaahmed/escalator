64ee920fa7e7ec62b2881b77577bdd0dbc760bc8
junaidzamaahmed <junaidahmedzama@gmail.com>
2024-12-03 23:26:46 +0600

    misc: fixing production code v1.4

--- server/src/utils/generateJWT.ts
+++ server/src/utils/generateJWT.ts
@@ -1,22 +1,16 @@
-import express, {
-  Request,
-  Response,
-  NextFunction,
-  RequestHandler,
-} from "express";
-import jwt, { JwtPayload } from "jsonwebtoken";
+import * as jwt from "jsonwebtoken";
 
 const secretKey = process.env.SECRET_KEY;
 const refreshSecretKey = process.env.REFRESH_SECRET_KEY;
 
-export function generateAccessToken(payload: JwtPayload) {
+export function generateAccessToken(payload: jwt.JwtPayload) {
   const token = jwt.sign(payload, secretKey!, {
     expiresIn: process.env.JWT_EXPIRES_IN,
   });
   return token;
 }
 
-export function generateRefreshToken(payload: JwtPayload) {
+export function generateRefreshToken(payload: jwt.JwtPayload) {
   const token = jwt.sign(payload, refreshSecretKey!, {
     expiresIn: process.env.REFRESH_JWT_EXPIRES_IN,
   });
